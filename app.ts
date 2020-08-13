55c75c6f033b567da451fd2a22681e186c77650c
Janos Maitner <janos.maitner@plus10.de>
2020-08-13 12:07:53 +0200

    test

---
+++ app.ts
@@ -0,0 +1,12 @@
+import { serve } from "https://deno.land/std@0.57.0/http/server.ts";
+import { parse } from 'https://deno.land/std/flags/mod.ts';
+
+const { args } = Deno;
+const DEFAULT_PORT = 8000;
+const argPort = parse(args).port;
+
+const s = serve({ port: argPort ? Number(argPort) : DEFAULT_PORT });
+
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
+}
